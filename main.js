032d3bdb0890548a3e367d6fa7242b7b043b9d43
hsmgebaly <hsmgebaly@gmail.com>
2022-11-09 03:04:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// Pricing toggle buttons
+var btn = document.getElementById("btn");
+
+function leftClick() {
+  btn.style.left = "0";
+}
+
+function rightClick() {
+  btn.style.left = "110px";
+}
